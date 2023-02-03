b850244531d60493267947aa65a440eca0510fdb
ndhuy21 <huytinhoc21@gmail.com>
2023-02-03 20:17:18 +0700

    add title and event

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -5,26 +5,38 @@ import searchVideo from "@/src/utils/search-videos";
 import { Container } from "@mui/material";
 import { GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { event } from "@/src/lib/ga";
+import Head from "next/head";
 
 const SearchPage = ({ videos, count }: { videos: Video[]; count: number }) => {
   const router = useRouter();
 
+  useEffect(() => {
+    event({ action: "search", params: { query: router.query.q } });
+  }, [router.query.q]);
+
   return (
-    <Container>
-      <VideoListPage
-        title={
-          router.query.q
-            ? `Kết quả tìm kiếm cho "${router.query.q}"`
-            : "Hãy nhập nội dung vào ô tìm kiếm nhé"
-        }
-        videos={videos}
-        page={Number(router.query.page || "1")}
-        pageCount={Math.ceil(count / VIDEO_PER_PAGE)}
-        onPageChange={(page) =>
-          router.push({ query: { ...router.query, page } })
-        }
-      />
-    </Container>
+    <>
+      <Head>
+        <title>{`"${router.query.q}" Tìm kiếm với ClipShow`}</title>
+      </Head>
+      <Container>
+        <VideoListPage
+          title={
+            router.query.q
+              ? `Kết quả tìm kiếm cho "${router.query.q}"`
+              : "Hãy nhập nội dung vào ô tìm kiếm nhé"
+          }
+          videos={videos}
+          page={Number(router.query.page || "1")}
+          pageCount={Math.ceil(count / VIDEO_PER_PAGE)}
+          onPageChange={(page) =>
+            router.push({ query: { ...router.query, page } })
+          }
+        />
+      </Container>
+    </>
   );
 };
 
