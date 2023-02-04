fafb2d357e884855ecce8a945011525326904721
ndhuy21 <huytinhoc21@gmail.com>
2023-02-04 20:54:08 +0700

    add adstera social ads, juicyads verify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -61,6 +61,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   gtag('js', new Date());
   gtag('config', 'G-29J3M10LK9'); `}
       </Script>
+      <Script
+        async
+        type="text/javascript"
+        src="//pl18398861.highcpmrevenuenetwork.com/fa/cc/3d/facc3d7bdb9973d0593846fd1180aee3.js"
+      ></Script>
     </React.Fragment>
   );
 }
